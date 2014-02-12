a09464f96289d803361dbc599ed10fdf171a874f
Anton Kalinin <antnkalinin@gmail.com>
2014-02-12 23:58:49 +0700

    main.js refactor, rename class html elements.

--- js/main.js
+++ js/main.js
@@ -1,45 +1,26 @@
-var oll = $(document).ready(function(){
- //   $('.close_img').bind('click', function callback(eventObject) {
- //       var m = this; // dom element
- //       alert(m);
- //       m.className = ('q1112356565656564');
-  //  });
-    $('#add').bind('click', start);       //РАБОЧИЙ ВАРИАНТ
-    $('#get').bind('click', ajax);       //РАБОЧИЙ ВАРИАНТ
+$(document).ready(function () {
 
-    $('.clear_checkbox_list').bind('click', clear_checkbox_list);
+	$('#add').bind('click', function(){
+		var m = $('#index').val();
+		$('#checkbox-list').append('' +
+						'<div class="wq">' +
+						'<label class="checkbox">' +
+						'<input type="checkbox" />' + m + '' +
+						'</label>' +
+						'<i class="icon-remove close-img"></i>' +
+						'</div>'
+		);
+		removeTwit();
+	});
+
+	$('.clear-checkbox-list').bind('click', function() {
+		$('#checkbox-list').replaceWith('<form id="checkbox-list" class="form-inline"></form>');
+	});
+
+	function removeTwit() {
+		$('.close-img').bind('click', function () {
+			$(this).closest("div").remove();
+		});
+	}
+
 });
-
-function clear_checkbox_list() {
-    $('#checkbox_list').replaceWith('<form id="checkbox_list" class="form-inline"></form>');
-}
-
-function find_close () { // after add new checkbox find them
-    $('.close_img').bind('click', function callback(eventObject) {
-        var m = this; // dom element
-        $(m).closest("div").remove();  //
-    });
-}
-
-function start() {  // add new element in checkbox_list
-    var m = document.getElementById('index').valueOf();
-    $(document).ready(function(){
-        $('#checkbox_list').append('' +
-            '<div class="wq">' +
-            '<label class="checkbox">' +
-            '   <input type="checkbox" />' + m.value + '' +
-            '</label>' +
-            '<i class="icon-remove close_img"></i>' +
-            '</div>'
-        );
-        find_close();
-    });
-}
-
-function ajax() {
-    $.ajax({
-        type: "GET",
-        url: "api.twitter.com/1.1/statuses/show.json?id=210462857140252672",
-        dataType: "json"
-    });
-}
